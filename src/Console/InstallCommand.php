e02f5896c8486a90d2718afe496d1ad97151306d
Taylor Otwell <taylor@laravel.com>
2022-05-05 13:44:16 -0500

    formatting

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -156,13 +156,11 @@ class InstallCommand extends Command
      */
     protected function configurePhpUnit()
     {
-        $file = $this->laravel->basePath('phpunit.xml');
-
-        if (! file_exists($file)) {
-            $file = $this->laravel->basePath('phpunit.xml.dist');
+        if (! file_exists($path = $this->laravel->basePath('phpunit.xml'))) {
+            $path = $this->laravel->basePath('phpunit.xml.dist');
         }
 
-        $phpunit = file_get_contents($file);
+        $phpunit = file_get_contents($path);
 
         $phpunit = preg_replace('/^.*DB_CONNECTION.*\n/m', '', $phpunit);
         $phpunit = str_replace('<!-- <env name="DB_DATABASE" value=":memory:"/> -->', '<env name="DB_DATABASE" value="testing"/>', $phpunit);
