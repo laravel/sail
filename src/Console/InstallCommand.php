3ba62ee89be69498807b2353f17dc8b8a736c416
jf-prevost <jfprevost@bluewin.ch>
2022-11-19 17:22:47 +0000

    fix issue #492
    php artisan sail:install --with=none not handled properly

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -220,13 +220,24 @@ class InstallCommand extends Command
             return;
         }
 
+	    // Pull images only if any needed
+	    if (count($services) > 0)
+	    {
+	        $status = $this->runCommands([
+                './vendor/bin/sail pull '.implode(' ', $services),
+            ]);
+            
+            if ($status === 0) {
+                $this->info('Sail images installed successfully.');
+            }
+	    }
+
         $status = $this->runCommands([
-            './vendor/bin/sail pull '.implode(' ', $services),
             './vendor/bin/sail build',
         ]);
 
         if ($status === 0) {
-            $this->info('Sail images installed successfully.');
+            $this->info('Sail build successful.');
         }
     }
 
