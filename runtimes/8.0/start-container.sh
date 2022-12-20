4e6052c0071f10877b30da0ddf22972b826f984d
arielm-cloudbuzz <110726427+arielm-cloudbuzz@users.noreply.github.com>
2022-12-20 12:34:04 +0200

    Update and rename start-container to start-container.sh

--- runtimes/8.0/start-container.sh
+++ runtimes/8.0/start-container.sh
@@ -2,16 +2,14 @@
 
 if [ ! -z "$WWWUSER" ]; then
     usermod -u $WWWUSER sail
-fi
 
-if [ ! -d /.composer ]; then
-    mkdir /.composer
-fi
-
-chmod -R ugo+rw /.composer
-
-if [ $# -gt 0 ]; then
+elif [ ! -d /.composer ]; then
+    mkdir /.composer &&
+    chmod -R ugo+rw /.composer
+    
+elif [ $# -gt 0 ]; then
     exec gosu $WWWUSER "$@"
+    
 else
     exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 fi
