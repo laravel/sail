437a864149674e8dd857a3eb53bb9365d0e123ec
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2024-03-08 00:45:43 +0300

    Update the .env with mailpit port

--- src/Console/Concerns/InteractsWithDockerComposeServices.php
+++ src/Console/Concerns/InteractsWithDockerComposeServices.php
@@ -186,6 +186,7 @@ trait InteractsWithDockerComposeServices
 
         if (in_array('mailpit', $services)) {
             $environment = preg_replace("/^MAIL_HOST=(.*)/m", "MAIL_HOST=mailpit", $environment);
+            $environment = preg_replace("/^MAIL_PORT=(.*)/m", "MAIL_PORT=1025", $environment);
         }
 
         file_put_contents($this->laravel->basePath('.env'), $environment);
