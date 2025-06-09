ddac0b98e33ebdb9e512bdf8c56803560c397774
Lucas Grzegorczyk <furai@thd.vg>
2025-06-09 08:26:52 +0200

    Rename service name laravel.test to laravel-test

--- src/Console/Concerns/InteractsWithDockerComposeServices.php
+++ src/Console/Concerns/InteractsWithDockerComposeServices.php
@@ -70,14 +70,14 @@ trait InteractsWithDockerComposeServices
 
         // Prepare the installation of the "mariadb-client" package if the MariaDB service is used...
         if (in_array('mariadb', $services)) {
-            $compose['services']['laravel.test']['build']['args']['MYSQL_CLIENT'] = 'mariadb-client';
+            $compose['services']['laravel-test']['build']['args']['MYSQL_CLIENT'] = 'mariadb-client';
         }
 
-        // Adds the new services as dependencies of the laravel.test service...
-        if (! array_key_exists('laravel.test', $compose['services'])) {
-            $this->warn('Couldn\'t find the laravel.test service. Make sure you add ['.implode(',', $services).'] to the depends_on config.');
+        // Adds the new services as dependencies of the laravel-test service...
+        if (! array_key_exists('laravel-test', $compose['services'])) {
+            $this->warn('Couldn\'t find the laravel-test service. Make sure you add ['.implode(',', $services).'] to the depends_on config.');
         } else {
-            $compose['services']['laravel.test']['depends_on'] = collect($compose['services']['laravel.test']['depends_on'] ?? [])
+            $compose['services']['laravel-test']['depends_on'] = collect($compose['services']['laravel-test']['depends_on'] ?? [])
                 ->merge($services)
                 ->unique()
                 ->values()
