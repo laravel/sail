4ab03a05d9f32d2c021c1cdf16900753a067cbef
Hafez Divandari <hafezdivandari@gmail.com>
2025-12-03 15:19:02 +0330

    check empty MYSQL_USER

--- database/mysql/create-testing-database.sh
+++ database/mysql/create-testing-database.sh
@@ -2,5 +2,10 @@
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS testing;
+EOSQL
+
+if [ -n "$MYSQL_USER" ]; then
+mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
     GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
 EOSQL
+fi
